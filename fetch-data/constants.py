d721dac2e8b925f1f57e43b90d67629014c69900
NapatzZ <Napat559977@outlook.co.th>
2026-03-30 16:22:06 +0700

    add: constants for fetch process

---
+++ fetch-data/constants.py
@@ -0,0 +1,11 @@
+# Product Identification Constants
+# From TalaadThai.com
+
+CORN_PRODUCTS = {
+    206: "ข้าวโพดหวาน – เบอร์กลาง",
+    216: "ข้าวโพดหวาน – เบอร์เล็ก",
+    182: "ข้าวโพดหวาน – เบอร์ใหญ่"
+}
+
+API_URL = "https://svc-center-ext-tlt-corp-prod-service.talaadthai.com/v1/ext/product/ProductPricingGraphSummary"
+BASE_URL = "https://talaadthai.com/products/corn-9827-2858"
